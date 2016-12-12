4b2cd58930b69cec0b6611b7a7cdc56232703b39
UnderPM <fake@mail.com>
2016-12-11 21:21:52 -0800

    Added DrawRect and DrawRectDim

--- Engine/Graphics.h
+++ Engine/Graphics.h
@@ -56,6 +56,10 @@ public:
 		PutPixel( x,y,{ unsigned char( r ),unsigned char( g ),unsigned char( b ) } );
 	}
 	void PutPixel( int x,int y,Color c );
+	void DrawRect(int x0, int y0, int x1, int y1, Color c);
+	void DrawRectDim(int x0, int y0, int width, int heigth, Color c) {
+		DrawRect(x0, y0, x0 + width, y0 + heigth, c);
+	}
 	~Graphics();
 private:
 	Microsoft::WRL::ComPtr<IDXGISwapChain>				pSwapChain;
