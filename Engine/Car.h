ddfda1fd3fe1e06c0012cfa62d0ba6af08634fab
UnderPM <UnderPM@users.noreply.github.com>
2017-01-17 10:36:29 -0800

    Added Car Class; Changed Tree Drawing code and class init

---
+++ Engine/Car.h
@@ -0,0 +1,8 @@
+#pragma once
+
+
+class Car {
+public:
+	bool isStaged;
+	bool isPreStaged;
+};
--- Engine/Tree.h
+++ Engine/Tree.h
@@ -5,4 +5,9 @@ public:
 	Tree(Graphics& gfx);
 private:
 	void DrawPro(int x, int y, Graphics& gfx);
+	void DrawPreStage(int x, int y, Graphics& gfx, Color c);
+	void DrawStage(int x, int y, Graphics& gfx, Color c);
+	void DrawProAmber(int x, int y, Graphics& gfx, Color c);
+	void DrawGreenLight(int x, int y, Graphics& gfx, Color c);
+	void DrawRedLight(int x, int y, Graphics& gfx, Color c);
 };
